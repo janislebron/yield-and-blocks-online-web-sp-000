7fa9c4a3b8c82feca94dffc043517a74fa94cc98
Janis Lebron <janislebron@gmail.com>
2020-05-18 00:18:59 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,14 +1,4 @@
 def hello_t(array)
-  i = 0
-
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
-end
-
-# call your method here!
-def hello_t(array)
   if block_given?
     i = 0
 
